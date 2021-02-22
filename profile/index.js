2e3b30b3a6c7df1152803bc35382b49f84934e7f
moli <1963813583@qq.com>
2021-02-22 21:38:34 +0800

    第十次提交

---
+++ profile/index.js
@@ -0,0 +1,19 @@
+const navMenuItems =document.querySelectorAll("#nav-menu a")
+const indicator = document.querySelector(".indicator");
+function handleMenuItemClick(target){
+    navMenuItems.forEach(item => {
+        item.classList.remove("active");
+        item.style="";
+    });
+ target.classList.add('active');
+indicator.style.width=`${target.offsetWidth}px`;
+indicator.style.left=`${target.offsetLeft}px`;
+const currentSection = document.querySelector(".active-section");
+currentSection.classList.remove("active-section");
+const newCurrentSection = document.querySelector(`.${target.getAttribute("data-rel")}`);
+newCurrentSection.classList.add("active-section")
+}
+navMenuItems.forEach(item => {
+    item.addEventListener("click",e =>handleMenuItemClick(e.target));
+ item.classList.contains('active') && handleMenuItemClick(item)   
+})
